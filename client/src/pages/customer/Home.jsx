d08423c7927f52a0a9c8534b679ae21ecd97c263
paul2k724-web <paula@pauldevverse.com>
2026-02-20 21:42:31 +0530

    Fix white screen - simplify CSS and add null checks

--- client/src/pages/customer/Home.jsx
+++ client/src/pages/customer/Home.jsx
@@ -16,7 +16,7 @@ import {
 
 const Home = () => {
   const dispatch = useDispatch()
-  const { services } = useSelector((state) => state.service)
+  const { services = [] } = useSelector((state) => state.service || {})
   const { user } = useSelector((state) => state.auth)
 
   useEffect(() => {
